baed11de998c40fc65790761620f5adf5722f42f
HMiesbauer <miesbah@gmail.com>
2024-04-06 15:31:48 -0700

    add corrected javascript, HTML

--- Notebooks/run2.js
+++ Notebooks/run2.js
@@ -1,79 +1,52 @@
+
+let movies = []
+
 // Get data from file
+
 function fetchDataFromCSV() {
-    return fetch('movie_data.csv').then(response => response.text()).then(text => {
-        const rows = text.split('\n').slice(1); // Skip header row
-            const movies = rows.map(row => {
-                const columns = row.split(',');
-                return { 
-                    imdb_id: columns[0],
-                    id: columns[1],
-                    adult: columns[2],
-                    belongs_to_collection: columns[3],
-                    genres: columns[4],
-                    original_language: columns[5],
-                    original_title: columns[6],
-                    overview: columns[7],
-                    popularity: columns[8],
-                    release_date: columns[9],
-                    revenue: columns[10],
-                    runtime: columns[11],
-                    status: columns[12],
-                    title: columns[13],
-                    video: columns[14],
-                    vote_average: columns[15],
-                    vote_count: columns[16]
-                    
-                };
-                
-            });
-            console.log("HELLOOO:", movies);
-            return movies;
-           
-        })};
-// create drop down box
-        function createDropDown(movies){
+    return d3.csv('movie_data.csv');
+}
+
+ // create drop down box
+function createDropDown(movies){
 
         let genres = movies.map(movie => movie.genres);
         let customerChoice = [...new Set(genres)];
+        let dropdown = d3.select("#movieDropDown");
 
-        // console.log("hello:" , customerChoice); this is returning customerChoice
-
-           d3.select("#movieDropDown")
-           .selectAll("option")
+           dropdown.selectAll("option")
            .data(customerChoice)
            .enter()
            .append("option")
            .text(d => d)
            .attr("value", d => d);
            
-     }
+           dropdown.on("change", function() {
+            updateChart(this.value);
+        });
+    };
 
- //  check if drop down box working
-   fetchDataFromCSV()
-         .then(movies => createDropDown(movies))
-         .catch(error => console.error(error));
-
-    
+   
  // change bar chart based on selection
  function updateChart(selectedGenre){
-    // console.log("hi:", selectedGenre) selectedGenre is getting returned
-    fetchDataFromCSV().then(movie => {
-        // filter
-        let movieGenre = movie.filter(movie => movie.genres === selectedGenre);
-        console.log("is this printing?:", movieGenre.map(movie => movie.popularity)); 
-        // sort
+
+// filter
+        const movieGenre = movies.filter(movie => movie.genres === selectedGenre);
+
+// sort
         movieGenre.sort((a, b) => parseFloat(b.popularity) - parseFloat(a.popularity));
-        // get top 10 movies
+
+// get top 10 movies
         let topTen = movieGenre.slice(0, 10);
-        // console.log("hi hi hi:", topTen)
-        // extract titles and votes
+
+// get movie titles and popularity
         let movieTitles = topTen.map(movie => movie.title);
         let popularityValues = topTen.map(movie => parseFloat(movie.popularity));
 
-    // create bar chart
+// create bar chart
     let trace1 = [{
-        x: popularityValues,
-        y: movieTitles,
+        x: movieTitles,
+        y: popularityValues ,
         type: "bar",
         orientation: "h"
     }];
@@ -85,15 +58,14 @@ function fetchDataFromCSV() {
 
 };
     Plotly.newPlot("bar", trace1, layout1);
-    });
- }
 
-// on click change chart -- this isn't working
-document.getElementById("movieDropDown").addEventListener('change', function(event) {
-let selectedGenre = event.target.value;
-// console.log("does this work?:", selectedGenre)
-updateChart(selectedGenre);
-});
+};
 
-// start point of chart
-updateChart('Action');
+// create drop down, and update chart
+fetchDataFromCSV()
+    .then(data => {
+        movies = data;
+        createDropDown(movies);
+        updateChart('Action'); 
+    })
+    .catch(error => console.error(error));
